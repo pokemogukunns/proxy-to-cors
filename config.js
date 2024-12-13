b9875a9c1e458f943cf3eefb26de201a5d6d62df
pokemogukunns <nattyannkunndesu@icloud.com>
2024-12-13 09:24:03 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 exports.port = process.env.PORT || 3000;
-exports.enable_logging = false;
-exports.fetch_regex = /^\/fetch\/(.*)$/; // The URL to look for when parsing the request.
-exports.proxy_request_timeout_ms = 10000; // The lenght of time we'll wait for a proxy server to respond before timing out.
-exports.max_request_length = 100000; // The maximum length of characters allowed for a request or a response.
+exports.enable_logging = true;
+exports.fetch_regex = /^\/(.*)$/; // リクエストを解析するときに探すURL。
+exports.proxy_request_timeout_ms = 10000; // タイムアウトする前に、プロキシサーバーが応答するのを待つ時間。
+exports.max_request_length = 10737418240; // 要求または応答に許可される最大文字長。
 exports.enable_rate_limiting = true;
-exports.max_requests_per_second = 10; // The maximum number of requests per second to allow from a given IP.
-exports.blacklist_hostname_regex = /^(10\.|192\.|127\.|localhost$)/i; // Good for limiting access to internal IP addresses and hosts.
+exports.max_requests_per_second = 10; // 特定のIPから許可される1秒あたりの最大リクエスト数。
+exports.blacklist_hostname_regex = /^(10\.|192\.|127\.|localhost$)/i; // 内部IPアドレスとホストへのアクセスを制限するのに適しています。
 exports.cluster_process_count = Number(process.env.CLUSTER_PROCESS_COUNT) || require("os").cpus().length;
